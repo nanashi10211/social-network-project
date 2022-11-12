141dfe42ea52f5df453a26312d89f228a52f4318
nanashi10211 <chakma.kalin10211@gmail.com>
2022-11-12 15:03:06 +0600

    login & register page design added

--- login.php
+++ login.php
@@ -18,18 +18,59 @@
     <!-- content row start -->
     <div class="row">
      
-        <!-- main content -->
-        <div class="offset-3 col-md-5 mid-content">
-            <div class="mid-content-box">
-               <!-- create post form system -->
-               <?php getCreatePostForm(); ?>
-               
-                <!-- all posts -->
-               <?php getRenderAllPosts(); ?>
+         <!-- main content -->
+         <div class="offset-1 col-md-10 mid-content">
+            <div class="mid-content-box ">
+             
+             
+                <div class="form-page-container login-box ">
+                    <!-- side content box -->
+                    <div class="side-content-box">
+                        <!-- <img src="./public/images/start.jpg" alt="image"> -->
+                        <h1>Welcome to <br> <span> POPCORN </span></h1>
+                        <p>Register <a href="register">here</a>  if you don't have an account</p>
+                    </div>
+                       <!-- register form -->
+                    <form action="#" method="post">
+                        <h1 class="form-header">Login</h1>
+                        <!-- single field -->
+                        <div class="form-field">
+                            <!-- <label class="label"  for="username">username:</label> -->
+                            <div class="input-box">
+                                <input type="text" name="username" id="username">
+                                <p class="help-text">
+                                    Enter a username or email
+                                </p>
+                            </div>
 
+                        </div>
+                        <!-- single field end -->
+                        
+                        
+                         <!-- single field -->
+                         <div class="form-field">
+                            <!-- <label class="label"  for="password">Password:</label> -->
+                            <div class="input-box">
+                                <input type="text" name="password" id="password">
+                                <p class="help-text">
+                                    Enter your password
+                                </p>
+                            </div>
+                        </div>
+                       
+                        <!-- single field -->
+                        <div class="form-field">
+                           
+                            <div class="input-box">
+                                <button class="form-btn" type="submit">login</button>
+                            </div>
+
+                        </div>
+                        <!-- single field end -->
+                    </form>
+                </div>
             </div>
         </div>
-
      
     </div>
     <!-- content row end -->
--- register.php
+++ register.php
@@ -18,14 +18,94 @@
     <div class="row">
      
         <!-- main content -->
-        <div class="offset-3 col-md-5 mid-content">
+        <div class="offset-1 col-md-10 mid-content">
             <div class="mid-content-box">
-               <!-- create post form system -->
-               <?php getCreatePostForm(); ?>
-               
-                <!-- all posts -->
-               <?php getRenderAllPosts(); ?>
+             
+             
+                <div class="form-page-container">
+                    <!-- side content box -->
+                    <div class="side-content-box">
+                        <!-- <img src="./public/images/start.jpg" alt="image"> -->
+                        <h1>Welcome to <br> <span> POPCORN </span></h1>
+                        <p>Login <a href="login">here</a>  if you already have an account</p>
+                    </div>
+                       <!-- register form -->
+                    <form action="#" method="post">
+                        <h1 class="form-header">Sigin-up</h1>
+                        <!-- single field -->
+                        <div class="form-field">
+                            <label class="label"  for="name">Name:</label>
+                            <div class="input-box">
+                                <input type="text" name="name" id="name">
+                                <p class="help-text">
+                                    Enter a name that will show as a display name
+                                </p>
+                            </div>
 
+                        </div>
+                        <!-- single field end -->
+                         <!-- single field -->
+                         <div class="form-field">
+                            <label class="label"  for="username">Username:</label>
+                            <div class="input-box">
+                                <input type="text" name="username" id="username">
+                                <p class="help-text">
+                                    create unique username, username only contain alphabet
+                                </p>
+
+                            </div>
+    
+                        </div>
+                        <!-- single field end -->
+                         <!-- single field -->
+                         <div class="form-field">
+                            <label class="label"  for="email">Email:</label>
+                            <div class="input-box">
+                                <input type="email" name="email" id="email">
+                                <p class="help-text">
+                                    Enter your presidency E-Mail (other email dose not work here)
+                                </p>
+                            </div>
+
+                        </div>
+                        <!-- single field end -->
+                         <!-- single field -->
+                         <div class="form-field">
+                            <label class="label"  for="password">Password:</label>
+                            <div class="input-box">
+                                <input type="text" name="password" id="password">
+                                <p class="help-text">
+                                    Password at least 8 character long and must contain 1 number, 1 uppercase, 1 lowercase & 1 symbolic character
+                                </p>
+                            </div>
+                        </div>
+                        <!-- single field end -->
+                         <!-- single field -->
+                         <div class="form-field">
+                            <label class="label"  for="confirm-password">confirm-password:</label>
+                            <div class="input-box">
+                                <input type="text" name="confirm-password" id="confirm-password">
+                                <p class="help-text">
+                                  Confirm possword must match with password
+                                </p>
+                            </div>
+
+                        </div>
+                        <!-- single field end -->
+                         <!-- single field -->
+                         <div class="form-field">
+                            <div class="label">
+                               
+                            </div>
+                            <!-- placeholder -->
+                            <div class="input-box">
+                                <button class="form-btn" type="submit">sign-up</button>
+                            </div>
+
+                        </div>
+                        <!-- single field end -->
+                    </form>
+                </div>
             </div>
         </div>
 
