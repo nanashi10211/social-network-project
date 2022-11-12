78f68dbacd956c4d4680f6e003ebb1d2e92191fd
nanashi10211 <chakma.kalin10211@gmail.com>
2022-11-13 03:19:46 +0600

    construct login error message

--- login.php
+++ login.php
@@ -20,7 +20,6 @@
         }
         if(isset($_POST['password'])) {
             $username = $_POST['password'];
-            
         }
 
         if(strlen($username) > 0 && strlen($password) > 0) {
@@ -30,11 +29,13 @@
             } else {
                 if(verify_password($password, $res[0]['password'])) {
                     // @TODO login user session will start here
-                    
+
                 } else {
                     $error = true;
                 }
             }
+        } else {
+            $error = true;
         }
 
     }
@@ -71,11 +72,21 @@
                        <!-- register form -->
                     <form action="#" method="post">
                         <h1 class="form-header">Login</h1>
+                       <div class="form-field">
+                        <div class="input-box">
+                            <?php if($error) {
+                                echo '<div style="width: 100%; margin-top: 20px" class="alert alert-danger" role="alert">';
+                                echo 'Invalid user!! try again....'; 
+                                echo '</div>';
+                            }?>
+                        </div>
+                       </div>
+                       
                         <!-- single field -->
                         <div class="form-field">
                             <!-- <label class="label"  for="username">username:</label> -->
                             <div class="input-box">
-                                <input type="text" name="username" required id="username" placeholder="Enter your email or username">
+                                <input  type="text" name="username" required id="username" placeholder="Enter your email or username">
                                 <p class="help-text">
                                    
                                 </p>
@@ -89,7 +100,7 @@
                          <div class="form-field">
                             <!-- <label class="label"  for="password">Password:</label> -->
                             <div class="input-box">
-                                <input type="password" name="password" required id="password" placeholder="Enter your password">
+                                <input  type="password" name="password" required id="password" placeholder="Enter your password">
                                 <p class="help-text">
                                    
                                 </p>
@@ -101,9 +112,11 @@
                            
                             <div class="input-box">
                                 <input class="form-btn" name="submit" type="submit" value="Login" />
+                            
+                             
                             </div>
 
-
+                          
                         </div>
                         <!-- single field end -->
                     </form>
